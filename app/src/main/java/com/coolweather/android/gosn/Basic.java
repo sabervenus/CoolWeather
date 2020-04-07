c9bc53657da238af006bde7f79cdec15b2a8ae29
yy <857054961@qq.com>
2020-04-07 19:18:43 +0800

    Add fragment

---
+++ app/src/main/java/com/coolweather/android/gosn/Basic.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gosn;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityNmae;  //城市名
+
+    @SerializedName("id")
+    public String weatherId;  //天气编号
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updatetime;  //更新时间
+    }
+}
